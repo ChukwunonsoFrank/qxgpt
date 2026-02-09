55512cb707849b09ee0273d7ac8adaa7c9c5d6d5
Chukwunonso Frank <franknonso114@gmail.com>
2026-02-09 09:34:23 +0100

    wip

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -55,77 +55,77 @@ class Register extends Component
   public function register()
   {
     try {
-      // if ($this->gRecaptchaResponse === null) {
-      //   $this->dispatch(
-      //     "signup-error",
-      //     message: "Please confirm you are not a robot.",
-      //   )->self();
-      // }
-
-      // $recatpchaResponse = Http::get(
-      //   "https://www.google.com/recaptcha/api/siteverify",
-      //   [
-      //     "secret" => config("services.recaptcha.secret"),
-      //     "response" => $this->gRecaptchaResponse,
-      //   ],
-      // );
-
-      // $result = $recatpchaResponse->json();
-
-      // if (
-      //   $recatpchaResponse->successful() &&
-      //   $result["success"] == true
-      // ) {
-      $validated = $this->validate([
-        'name' => ['required', 'string', 'max:255'],
-        'email' => [
-          'required',
-          'string',
-          'lowercase',
-          'email',
-          'max:255',
-          'unique:' . User::class,
-        ],
-        'password' => [
-          'required',
-          'string',
-          'confirmed',
-          Rules\Password::defaults(),
-        ],
-        'termsAndPrivacyPolicyAccepted' => 'accepted',
-      ]);
-
-      unset($validated['termsAndPrivacyPolicyAccepted']);
-
-      $refCode = $this->ref ?? $this->ref_code;
-      if ($refCode && ! User::where('referral_code', $refCode)->exists()) {
-        $this->dispatch('signup-error', message: 'Invalid referral code.')->self();
-
-        return;
+      if ($this->gRecaptchaResponse === null) {
+        $this->dispatch(
+          "signup-error",
+          message: "Please confirm you are not a robot.",
+        )->self();
       }
 
-      /**
-       * Attempt to send login code to user's email.
-       */
-      $loginCode = substr(str_shuffle('0123456789'), 0, 6);
-      Notification::route('mail', $validated['email'])->notify(
-        new LoginCodeRequested($loginCode),
+      $recatpchaResponse = Http::get(
+        "https://www.google.com/recaptcha/api/siteverify",
+        [
+          "secret" => config("services.recaptcha.secret"),
+          "response" => $this->gRecaptchaResponse,
+        ],
       );
 
-      $this->redirectRoute('register.verifylogincode', [
-        'name' => $validated['name'],
-        'email' => $validated['email'],
-        'password' => $validated['password'],
-        'hash' => Hash::make($loginCode),
-        'ref' => $this->ref ?? $this->ref_code,
-      ]);
-      // } else {
-      //   $this->dispatch(
-      //     "signup-error",
-      //     message: "Please confirm you are not a robot.",
-      //   )->self();
-      //   return redirect()->back();
-      // }
+      $result = $recatpchaResponse->json();
+
+      if (
+        $recatpchaResponse->successful() &&
+        $result["success"] == true
+      ) {
+        $validated = $this->validate([
+          'name' => ['required', 'string', 'max:255'],
+          'email' => [
+            'required',
+            'string',
+            'lowercase',
+            'email',
+            'max:255',
+            'unique:' . User::class,
+          ],
+          'password' => [
+            'required',
+            'string',
+            'confirmed',
+            Rules\Password::defaults(),
+          ],
+          'termsAndPrivacyPolicyAccepted' => 'accepted',
+        ]);
+
+        unset($validated['termsAndPrivacyPolicyAccepted']);
+
+        $refCode = $this->ref ?? $this->ref_code;
+        if ($refCode && ! User::where('referral_code', $refCode)->exists()) {
+          $this->dispatch('signup-error', message: 'Invalid referral code.')->self();
+
+          return;
+        }
+
+        /**
+         * Attempt to send login code to user's email.
+         */
+        $loginCode = substr(str_shuffle('0123456789'), 0, 6);
+        Notification::route('mail', $validated['email'])->notify(
+          new LoginCodeRequested($loginCode),
+        );
+
+        $this->redirectRoute('register.verifylogincode', [
+          'name' => $validated['name'],
+          'email' => $validated['email'],
+          'password' => $validated['password'],
+          'hash' => Hash::make($loginCode),
+          'ref' => $this->ref ?? $this->ref_code,
+        ]);
+      } else {
+        $this->dispatch(
+          "signup-error",
+          message: "Please confirm you are not a robot.",
+        )->self();
+        return redirect()->back();
+      }
     } catch (\Exception $e) {
       $this->dispatch('signup-error', message: $e->getMessage())->self();
     }
