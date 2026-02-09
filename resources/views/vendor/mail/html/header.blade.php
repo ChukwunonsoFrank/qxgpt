d533e4dfdab2f33772a6b7cb38749d3d9f8a8ec0
Chukwunonso Frank <franknonso114@gmail.com>
2026-02-09 10:09:26 +0100

    wip

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Yfxai')
-                <img width="100" src="{{ asset('wp-content/uploads/2023/05/yfxai-logo-dark.png') }}" alt="Yfxai Logo">
+            @if (trim($slot) === 'Qxgpt')
+                <img width="100" src="{{ asset('wp-content/uploads/2023/05/qxgpt.svg') }}" alt="Qxgpt Logo">
             @else
                 {{ $slot }}
             @endif
