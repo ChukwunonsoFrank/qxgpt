5546ffdfb0df6f5063985f3ce1088b0484dce86a
Chukwunonso Frank <franknonso114@gmail.com>
2026-02-12 23:44:22 +0100

    wip

--- app/Livewire/Dashboard/Robot.php
+++ app/Livewire/Dashboard/Robot.php
@@ -871,18 +871,45 @@ class Robot extends Component
 
       $roiPercentage = intval($this->strategy['max_roi']);
 
-      // if ($balanceToDebit === 'live_balance') {
-      //   $user = auth()->user();
-      //   $profitExceedDay = $user->profit_exceed_day;
+      $nanoPercentages = [7, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8];
+      $quantumPercentages = [10, 10.1, 10.2, 10.3, 10.4, 10.5, 10.6, 10.7, 10.8, 10.9, 11];
+      $optimalPercentages = [13, 13.1, 13.2, 13.3, 13.4, 13.5, 13.6, 13.7, 13.8, 13.9, 14];
+      $aggressivePercentages = [16, 16.1, 16.2, 16.3, 16.4, 16.5, 16.6, 16.7, 16.8, 16.9, 17];
+      // $exceedPercentages = [11, 11.1, 11.2, 11.3, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9, 12, 12.1, 12.2, 12.3, 12.4, 12.5, 12.6, 12.7, 12.8, 12.9, 13];
 
-      //   if ($profitExceedDay !== null && $profitExceedDay !== 0 && $profitExceedDay === Carbon::now()->dayOfWeek) {
-      //     $exceedPercentages = [11, 11.1, 11.2, 11.3, 11.4, 11.5, 11.6, 11.7, 11.8, 11.9, 12, 12.1, 12.2, 12.3, 12.4, 12.5, 12.6, 12.7, 12.8, 12.9, 13];
-      //     $roiPercentage = $exceedPercentages[array_rand($exceedPercentages)];
-      //   } elseif ($profitExceedDay === 0) {
-      //     $dailyPercentages = [8, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10];
-      //     $roiPercentage = $dailyPercentages[array_rand($dailyPercentages)];
-      //   }
-      // }
+      if ($balanceToDebit === 'live_balance') {
+        // $user = auth()->user();
+        // $profitExceedDay = $user->profit_exceed_day;
+
+        // if ($profitExceedDay !== null && $profitExceedDay !== 0 && $profitExceedDay === Carbon::now()->dayOfWeek) {
+        //   $roiPercentage = $exceedPercentages[array_rand($exceedPercentages)];
+        // } elseif ($profitExceedDay === 0) {
+        //   if ($this->strategy['name'] === 'Nano') {
+        //     $roiPercentage = $nanoPercentages[array_rand($nanoPercentages)];
+        //   }
+        //   if ($this->strategy['name'] === 'Quantum') {
+        //     $roiPercentage = $quantumPercentages[array_rand($quantumPercentages)];
+        //   }
+        //   if ($this->strategy['name'] === 'Optimal') {
+        //     $roiPercentage = $optimalPercentages[array_rand($optimalPercentages)];
+        //   }
+        //   if ($this->strategy['name'] === 'Aggressive') {
+        //     $roiPercentage = $aggressivePercentages[array_rand($aggressivePercentages)];
+        //   }
+        // }
+        if ($this->strategy['name'] === 'Nano') {
+          $roiPercentage = $nanoPercentages[array_rand($nanoPercentages)];
+        }
+        if ($this->strategy['name'] === 'Quantum') {
+          $roiPercentage = $quantumPercentages[array_rand($quantumPercentages)];
+        }
+        if ($this->strategy['name'] === 'Optimal') {
+          $roiPercentage = $optimalPercentages[array_rand($optimalPercentages)];
+        }
+        if ($this->strategy['name'] === 'Aggressive') {
+          $roiPercentage = $aggressivePercentages[array_rand($aggressivePercentages)];
+        }
+      }
 
       $profitLimit = ($roiPercentage / 100) * $this->normalizeAmount($amount);
       $currentBalance = auth()->user()->{$balanceToDebit};
